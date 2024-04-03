e998992ef8f725af590f7b76913e75b9b2c1e488
yanniao0827 <yanning0113niao@gmail.com>
2024-04-03 16:54:21 +0800

    我到底在幹嘛

--- 0403/practice.js
+++ 0403/practice.js
@@ -1,5 +1,8 @@
+// 記錄一個來自外部的JSON字串
 let data = '{"name": "John Doe", "age": 30, "city": "New York"}';
-
+// 把該JSON字串解析成JS
 let jsonObj = JSON.parse(data);
+// 轉成JS後增加一個屬性
 jsonObj.job = 'Engineer';
+// 最後轉換回JSON
 console.log(JSON.stringify(jsonObj));
